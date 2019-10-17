5caef0bf296ae14963800cb8fc5ed3938220e56d
sbx_user1051 <sbx_user1051@169.254.3.37>
2019-10-17 21:30:21 +0000

    Thu Oct 17 21:30:21 UTC 2019

--- src/BeFaster.App/Solutions/HLO/HelloSolution.cs
+++ src/BeFaster.App/Solutions/HLO/HelloSolution.cs
@@ -4,9 +4,8 @@ namespace BeFaster.App.Solutions.HLO
 {
     public static class HelloSolution
     {
-        public static string Hello(string friendName)
-        {
-            throw new SolutionNotImplementedException();
-        }
+        public static string Hello(string friendName) =>
+            "Hello world";
     }
 }
+
