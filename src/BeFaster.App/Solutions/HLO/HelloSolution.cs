7a10bd86b9e03bed68b6374fe01f9a8eeab9be36
sbx_user1051 <sbx_user1051@169.254.3.37>
2019-10-17 21:36:26 +0000

    Thu Oct 17 21:36:26 UTC 2019

--- src/BeFaster.App/Solutions/HLO/HelloSolution.cs
+++ src/BeFaster.App/Solutions/HLO/HelloSolution.cs
@@ -5,8 +5,9 @@ namespace BeFaster.App.Solutions.HLO
     public static class HelloSolution
     {
         public static string Hello(string friendName) =>
-            "Hello, World!";
+            $"Hello, {friendName}!";
     }
 }
+
 
 
