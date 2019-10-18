8186b0985cd59cf22fda3c18a8407f306661211d
sbx_user1051 <sbx_user1051@169.254.1.133>
2019-10-18 21:30:37 +0000

    Fri Oct 18 21:30:37 UTC 2019

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -24,8 +24,25 @@ namespace BeFaster.App.Solutions.CHK
             var aCount = skuToCountMapping.GetValueOrDefault('A');
             var bCount = skuToCountMapping.GetValueOrDefault('B');
             var eCount = skuToCountMapping.GetValueOrDefault('E');
+
+            var aAfter5Count = aCount % 5;
+            var aAfter3Count = aAfter5Count % 3;
+            var aPrice = aCount / 5 * 200 + aAfter5Count / 3 * 130 + aAfter3Count * 50;
+
+            var freeBCount = eCount / 2;
+            var pricedBCount = Math.Max(bCount - freeBCount, 0);
+            var pricedBAfter2Count = pricedBCount % 2;
+            var bPrice = pricedBCount / 2 * 45 + pricedBAfter2Count * 30;
+
+            return
+                aPrice +
+                bPrice +
+                skuToCountMapping.GetValueOrDefault('C') * 20 +
+                skuToCountMapping.GetValueOrDefault('D') * 15 +
+                eCount * 40;
         }
 
     }
 }
+
 
