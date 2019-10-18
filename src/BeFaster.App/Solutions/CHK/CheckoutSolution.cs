addf0d9bc4297ddb1d173c731afc116ddbabd43f
sbx_user1051 <sbx_user1051@169.254.206.9>
2019-10-18 09:08:18 +0000

    Fri Oct 18 09:08:18 UTC 2019

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -5,13 +5,43 @@ namespace BeFaster.App.Solutions.CHK
 {
     public static class CheckoutSolution
     {
-        private readonly Dictionary<char, List<(int Amount, int Price)>> _skuToAmountsAndPrices =
+        private static readonly Dictionary<char, List<(int Amount, int Price)>> SkuToAmountsAndPricesMapping =
             new Dictionary<char, List<(int Amount, int Price)>>
             {
-
+                { 
+                    'A',
+                    new List<(int Amount, int Price)>
+                    {
+                        (1, 50),
+                        (3, 130)
+                    }
+                },
+                {
+                    'B',
+                    new List<(int Amount, int Price)>
+                    {
+                        (1, 30),
+                        (2, 45)
+                    }
+                },
+                {
+                    'C',
+                    new List<(int Amount, int Price)>
+                    {
+                        (1, 20)
+                    }
+                },
+                {
+                    'D',
+                    new List<(int Amount, int Price)>
+                    {
+                        (1, 15)
+                    }
+                }
             };
         public static int ComputePrice(string skus)
         {
         }
     }
 }
+
