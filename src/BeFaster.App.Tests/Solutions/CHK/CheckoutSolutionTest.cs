20b3fa671654c9ec5bd83490e84fc670f065e578
sbx_user1051 <sbx_user1051@169.254.1.133>
2019-10-18 21:00:36 +0000

    Fri Oct 18 21:00:36 UTC 2019

--- src/BeFaster.App.Tests/Solutions/CHK/CheckoutSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/CHK/CheckoutSolutionTest.cs
@@ -6,10 +6,23 @@ namespace BeFaster.App.Tests.Solutions.CHK
     [TestFixture]
     public class CheckoutSolutionTest
     {
-        [TestCase("ABBAC", ExpectedResult = 165)]
         [TestCase("", ExpectedResult = 0)]
-        [TestCase("EBBAC", ExpectedResult = -1)]
+        [TestCase("AAA", ExpectedResult = 130)]
+        [TestCase("AAAAA", ExpectedResult = 200)]
+        [TestCase("AAAAAAA", ExpectedResult = 300)]
+        [TestCase("AAAAAAAA", ExpectedResult = 330)]
+        [TestCase("AAAAAAAAAA", ExpectedResult = 400)]
+        [TestCase("B", ExpectedResult = 30)]
+        [TestCase("BB", ExpectedResult = 45)]
+        [TestCase("ABBAC", ExpectedResult = 165)]
+        [TestCase("C", ExpectedResult = 20)]
+        [TestCase("D", ExpectedResult = 15)]
+        [TestCase("EE", ExpectedResult = 80)]
+        [TestCase("E", ExpectedResult = 40)]
+        [TestCase("EEB", ExpectedResult = 80)]
+        [TestCase("EEBB", ExpectedResult = 110)]
         public int ComputePrice(string skus) =>
             CheckoutSolution.ComputePrice(skus);
     }
 }
+
