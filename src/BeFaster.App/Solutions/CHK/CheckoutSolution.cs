468d59f24007d14b7dae60424863579435439500
sbx_user1051 <sbx_user1051@169.254.1.133>
2019-10-18 21:45:36 +0000

    Fri Oct 18 21:45:36 UTC 2019

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -9,7 +9,7 @@ namespace BeFaster.App.Solutions.CHK
     {
         public static int ComputePrice(string skus)
         {
-            if (skus == null || !skus.Matches("[A-E]*"))
+            if (skus == null || !skus.Matches("^[A-E]*$"))
             {
                 Console.WriteLine($"Illegal input [{nameof(skus)}={skus ?? "null"}]");
                 return -1;
@@ -44,5 +44,3 @@ namespace BeFaster.App.Solutions.CHK
 
     }
 }
-
-
