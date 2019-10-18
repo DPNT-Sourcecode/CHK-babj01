b0b4edca684a2c7abc30551f294d960995b15f80
sbx_user1051 <sbx_user1051@169.254.55.85>
2019-10-18 22:47:07 +0000

    Fri Oct 18 22:47:07 UTC 2019

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -14,8 +14,8 @@ namespace BeFaster.App.Solutions.CHK
                 {'B', new[] {(2, 45), (1, 30)}},
                 {'C', new[] {(1, 20)}},
                 {'D', new[] {(1, 15)}},
-                {'E', new[] {(1, 40) /*| 2 get one B free */}},
-                {'F', new[] {(1, 10) /*| 2 get one F free */}},
+                {'E', new[] {(1, 40)}},
+                {'F', new[] {(3, 20), (1, 10)}},
                 {'G', new[] {(1, 20)}},
                 {'H', new[] {(10, 80), (5, 45), (1, 10)}},
                 {'I', new[] {(1, 35)}},
@@ -23,14 +23,14 @@ namespace BeFaster.App.Solutions.CHK
                 {'K', new[] {(2, 150), (1, 80)}},
                 {'L', new[] {(1, 90)}},
                 {'M', new[] {(1, 15)}},
-                {'N', new[] {(1, 40) /*,et one M free */}},
+                {'N', new[] {(1, 40)}},
                 {'O', new[] {(1, 10)}},
                 {'P', new[] {(5, 200), (1, 50)}},
                 {'Q', new[] {(3, 80), (1, 30)}},
-                {'R', new[] {(1, 50) /*,et one Q free   */}},
+                {'R', new[] {(1, 50)}},
                 {'S', new[] {(1, 30)}},
                 {'T', new[] {(1, 20)}},
-                {'U', new[] {(1, 40) /*,et one U free  */}},
+                {'U', new[] {(4, 120), (1, 40)}},
                 {'V', new[] {(3, 130), (2, 90), (1, 50)}},
                 {'W', new[] {(1, 20)}},
                 {'X', new[] {(1, 90)}},
@@ -38,37 +38,38 @@ namespace BeFaster.App.Solutions.CHK
                 {'Z', new[] {(1, 50)}}
             };
 
+        private static readonly Dictionary<char, (char QualifyingSku, int Amount)> SkuToQualifyingSkuAndAmountMapping =
+            new Dictionary<char, (char, int)>
+            {
+                {'B', ('E', 2)},
+                {'Q', ('U', 3)},
+                {'M', ('N', 3)},
+                {'Q', ('R', 3)}
+            };
+
         public static int ComputePrice(string skus)
         {
             if (skus == null || skus.Matches("^[A-Z]*$"))
             {
                 Console.WriteLine($"Invalid input [{nameof(skus)}={skus ?? "null"}]");
+                return -1;
             }
 
             var sum = 0;
-            try
+            foreach (var skuAndCount in skus.GroupBy(_ => _).Select(_ => (_.Key, _.Count())))
             {
-                foreach (var skuAndCount in skus.GroupBy(_ => _).Select(_ => (_.Key, _.Count())))
+                var (sku, count) = skuAndCount;
+
+                foreach (var (amount, price) in SkuToAmountsAndPricesMapping[sku])
                 {
-                    var (sku, count) = skuAndCount;
+                    sum += count / amount * price;
+                    count %= amount;
 
-                    foreach (var (amount, price) in SkuToAmountsAndPricesMapping[sku])
-                    {
-                        sum += count / amount * price;
-                        count %= amount;
-
-                        if (count == 0) break;
-                    }
+                    if (count == 0) break;
                 }
+            }
 
-                return sum;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                return -1;
-            }
+            return sum;
         }
     }
-
 }
